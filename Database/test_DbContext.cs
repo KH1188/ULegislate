b04ab00105c923ff1c669fe5d133b63286fdab49
Julius Pallotta <65132245+KH1188@users.noreply.github.com>
2023-06-10 20:42:28 -0500

    Update test_DbContext.cs
    created dbsets for the file information

--- Database/test_DbContext.cs
+++ Database/test_DbContext.cs
@@ -2,12 +2,15 @@ using Microsoft.EntityFrameworkCore;
 
 public class test_DbContext : DbContext
 {
-    public test_DbContext(DbContextOptions<MyDbContext> options) : base(options)
+    public test_DbContext(DbContextOptions<test_DbContext> options) : base(options)
     {
     }
 
     // Define DbSet properties for your entities
-    public DbSet<File> Files { get; set; }
-    public DbSet<Directory> Directories { get; set; }
+    public DbSet<FileId> FileId { get; set; }
+    public DbSet<FileName> FileName { get; set; }
+    public DbSet<FilePath> FilePath { get; set; }
+    public DbSet<Metadata> Metadata { get; set; }
+    public DbSet<Status> Status { get; set; }
     // Add other DbSet properties as needed
 }
