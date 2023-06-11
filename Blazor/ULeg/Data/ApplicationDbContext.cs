13924381424a5c1f70e02bbbb170d49da93824da
Julius Pallotta <65132245+KH1188@users.noreply.github.com>
2023-06-10 20:43:59 -0500

    Update ApplicationDbContext.cs
    added public class for the database

--- Blazor/ULeg/Data/ApplicationDbContext.cs
+++ Blazor/ULeg/Data/ApplicationDbContext.cs
@@ -11,3 +11,18 @@ namespace ULeg.Data
         }
     }
 }
+
+public class test_DbContext : DbContext
+{
+    public test_DbContext(DbContextOptions<test_DbContext> options) : base(options)
+    {
+    }
+
+    // Define DbSet properties for your entities
+    public DbSet<FileId> FileId { get; set; }
+    public DbSet<FileName> FileName { get; set; }
+    public DbSet<FilePath> FilePath { get; set; }
+    public DbSet<Metadata> Metadata { get; set; }
+    public DbSet<Status> Status { get; set; }
+    // Add other DbSet properties as needed
+}
